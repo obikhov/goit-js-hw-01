3fa730b64ae94fb5469b3db552a8c521b6a1d38c
Vladislav <160542917+obikhov@users.noreply.github.com>
2024-04-25 22:25:45 +0300

    dz
    dz

---
+++ js/task-1.js
@@ -0,0 +1,25 @@
+function makeTransaction(quantity,pricePerDroid){
+    return `You ordered ${quantity} droids worth ${quantity*pricePerDroid} credits  `
+}
+function makeTransaction(quantity,pricePerDroid){
+    return `You ordered ${quantity} droids worth ${quantity*pricePerDroid} credits  `
+}
+function makeTransaction(quantity,pricePerDroid){
+    return `You ordered ${quantity} droids worth ${quantity*pricePerDroid} credits  `
+}
+
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+
+
+// function functionName(a,b){
+//     return `text ${a} then ${b}`
+
+//  }
+// console.log(functionName(1200,500))
+  
+
+ 
