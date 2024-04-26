ce09a001308d9b48fa5ba05f20b6e79c9a298115
Vladislav <160542917+obikhov@users.noreply.github.com>
2024-04-26 18:50:07 +0300

    js-1-2-3
    js-1-2-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,36 +1,11 @@
-function getElementWidth(content,padding,border){
+function getElementWidth(content, padding, border) {
     const contentWidth = parseFloat(content);
-    
-    
-const paddingWidth = parseFloat(padding);
-    
-    
-const borderWidth = parseFloat(border);
-const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-return totalWidth;
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+     return totalWidth;
 }
-function getElementWidth(content,padding,border){
-    const contentWidth = parseFloat(content);
-    
-    
-const paddingWidth = parseFloat(padding);
-    
-    
-const borderWidth = parseFloat(border);
-const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-return totalWidth;
-}
-function getElementWidth(content,padding,border){
-    const contentWidth = parseFloat(content);
-    
-    
-const paddingWidth = parseFloat(padding);
-    
-    
-const borderWidth = parseFloat(border);
-const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-return totalWidth;
-}
+// Перевірка роботи функції
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
